70813ce942015fa636b22a0110c9b73f27c8eb5b
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 03:59:37 +0000

    Full path to executable

--- .ci-scripts/check-code-samples.bash
+++ .ci-scripts/check-code-samples.bash
@@ -13,7 +13,8 @@ for file in *.pony; do
     percentage=$(((i*100)/files))
     echo -e "#$i Test $file … ($i/$files \u2192 $percentage %)"
     #docker run -v $(pwd):/src/main docker://ghcr.io/ponylang/ponyc:latest
-    ponyc "./code-samples/$file"
+    #ponyc "./code-samples/$file"
+    $HOME/.local/share/ponyup/bin/ponyc "./code-samples/$file"
     if [ $? -eq 0 ]; then
         echo -e "\e[1;32m\u2705 File fulfilled expectations\e[0m"
     else
