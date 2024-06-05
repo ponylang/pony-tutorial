0ac65f12fdf893f4d872e45ed157084f3a5e0dd2
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 20:19:41 +0200

    use pygments

--- lib/superfences_ponylang/__init__.py
+++ lib/superfences_ponylang/__init__.py
@@ -1,5 +1,16 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
 def format(source, language, css_class, options, md, classes=None, id_value='', attrs=None, **kwargs):
-    return '<pre class_name="ponylang %s %s", data-option="%s"><nav class="md-code__nav"><button class="md-code__button" title="Copy to clipboard" data-clipboard-target="#__code_1 &gt; code" data-md-type="copy"></button></nav><code>%s</code></pre>' % (language, class_name, options['opt'], html_escape(source))
+    return """
+    <pre class_name="ponylang %s %s", data-option="%s">
+        <nav class="md-code__nav">
+            <button class="md-code__button" title="Copy to clipboard" data-clipboard-target="#__code_1 &gt; code" data-md-type="copy"></button>
+        </nav>
+        <code>%s</code>
+    </pre>
+    """ % (language, class_name, options['opt'], html_escape(highlight(source, PythonLexer(), HtmlFormatter())))
 
 def validate(language: str, options: dict) -> bool:
     allowed_options = { "snippet", "dedent_subsections" } #lambda v: v in ENGINES
