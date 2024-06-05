35553553a6e06f4f0678e5ee3736333a80340e79
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 03:36:50 +0000

    Test

--- .ci-scripts/setup-ponyc.bash
+++ .ci-scripts/setup-ponyc.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-set -u
+#set -u
 
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
 ponyup update ponyc release
