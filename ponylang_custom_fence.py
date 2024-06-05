28952fea1a11e75f344c512116845883848232a3
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 18:45:16 +0200

    Use file in route directory (no module)

---
+++ ponylang_custom_fence.py
@@ -0,0 +1,5 @@
+def format(source, language, css_class, options, md, classes=None, id_value='', attrs=None, **kwargs):
+    return '<pre class_name="%s %s", data-option="%s"><nav class="md-code__nav"><button class="md-code__button" title="Copy to clipboard" data-clipboard-target="#__code_1 > code" data-md-type="copy"></button></nav><code>%s</code></pre>' % (language, class_name, options['opt'], html_escape(source))
+
+def validate(language: str, options: dict) -> bool:
+    return True # TODO allowed attributes and values
