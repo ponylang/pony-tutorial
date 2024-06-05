049c4528db602c22827f45b769f51587422c71f9
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 03:34:41 +0000

    Install ponyc via ponyup instead of docker

--- .ci-scripts/setup-ponyc.bash
+++ .ci-scripts/setup-ponyc.bash
@@ -1,17 +1,7 @@
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+#!/bin/bash
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+set -e
+set -u
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-docker pull docker://ghcr.io/ponylang/ponyc:latest
+sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
+ponyup update ponyc release
