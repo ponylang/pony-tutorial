c34ae9db16ab4c73c47dc067d8251e3d2ab4790c
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 20:11:57 +0200

    Add validation

--- lib/superfences_ponylang/__init__.py
+++ lib/superfences_ponylang/__init__.py
@@ -2,4 +2,8 @@ def format(source, language, css_class, options, md, classes=None, id_value='', 
     return '<pre class_name="ponylang %s %s", data-option="%s"><nav class="md-code__nav"><button class="md-code__button" title="Copy to clipboard" data-clipboard-target="#__code_1 &gt; code" data-md-type="copy"></button></nav><code>%s</code></pre>' % (language, class_name, options['opt'], html_escape(source))
 
 def validate(language: str, options: dict) -> bool:
-    return True # TODO allowed attributes and values
+    allowed_options = { "snippet", "dedent_subsections" } #lambda v: v in ENGINES
+    for opt in options.keys():
+        if opt not in allowed_options:
+            return False
+    return True
