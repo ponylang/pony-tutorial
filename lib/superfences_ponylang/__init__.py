ad2376280fc1b434823005f847039c319d26d1c6
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 20:21:31 +0200

    Add nav buttons

--- lib/superfences_ponylang/__init__.py
+++ lib/superfences_ponylang/__init__.py
@@ -7,6 +7,8 @@ def format(source, language, css_class, options, md, classes=None, id_value='', 
     <pre class_name="ponylang %s %s", data-option="%s">
         <nav class="md-code__nav">
             <button class="md-code__button" title="Copy to clipboard" data-clipboard-target="#__code_1 &gt; code" data-md-type="copy"></button>
+            <button class="md-code__button" title="Run in playground" data-md-type="run"></button>
+            <button class="md-code__button" title="Run in playground" data-md-type="inline"></button>
         </nav>
         <code>%s</code>
     </pre>
