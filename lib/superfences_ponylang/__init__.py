b113123ea24f0076e09ceefb726b573a67b14307
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-06 01:59:06 +0200

    even more debug

--- lib/superfences_ponylang/__init__.py
+++ lib/superfences_ponylang/__init__.py
@@ -10,6 +10,7 @@ from mkdocs.config import base
 from mkdocs.config import config_options as c
 from mkdocs.structure.pages import Page, _AbsoluteLinksValidationValue
 from mkdocs.utils.yaml import get_yaml_loader, yaml_load
+from mkdocs.utils.templates import TemplateContext
 
 import os
 
@@ -36,7 +37,7 @@ def format(source, language, css_class, options, md, classes=None, id_value='', 
                                 lines.append(line)
                     #source = str(lines)
                     source = '\n'.join(lines)
-                    source = str(base.Config.user_configs.__dict__) + str(options) + str(attrs) + str(classes) + str(kwargs)
+                    source = str(base.Config.user_configs.__dict__) + str(TemplateContext) + str(options) + str(attrs) + str(classes) + str(kwargs)
             else:
                 with open(os.getcwd() + "/code-samples/" + snippetPath, 'r') as f:
                     source = f.read()
