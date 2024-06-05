dbc1511e6bddf28492723d6d2822e0da582cd0ae
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 03:52:31 +0000

    Count failed files and use ponyc directly

--- .ci-scripts/check-code-samples.bash
+++ .ci-scripts/check-code-samples.bash
@@ -7,24 +7,24 @@ cd ./code-samples/
 files=$(ls | wc -l)
 echo "Check $files files …"
 failedFiles=()
-notRunnable=0
 i=0
 for file in *.pony; do
     ((i++))
     percentage=$(((i*100)/files))
     echo -e "#$i Test $file … ($i/$files \u2192 $percentage %)"
-    docker run -v $(pwd):/src/main docker://ghcr.io/ponylang/ponyc:latest
+    #docker run -v $(pwd):/src/main docker://ghcr.io/ponylang/ponyc:latest
+    ponyc "./code-samples/$file"
     if [ $? -eq 0 ]; then
         echo -e "\e[1;32m\u2705 File fulfilled expectations\e[0m"
     else
-        echo -e "\e[1;32m\u2705 File fulfilled expectations\e[0m"
+        echo -e "\e[1;31m\u274C File didn't fulfill expectations\e[0m"
+        failedFiles+=(file)
     fi
 done
-runnableFiles=$((files-notRunnable))
 if [ "${#failedFiles[@]}" != 0 ]; then
-    echo  -e "\e[1;31m💥 ${#failedFiles[@]}/$runnableFiles file(s) ($files total) had errors\e[0m"
+    echo  -e "\e[1;31m💥 ${#failedFiles[@]}/$files file(s) had errors\e[0m"
     exit 1
 else
-    echo -e "\e[1;32m🎉 All $files files ($runnableFiles runnable) were checked successfully\e[0m"
+    echo -e "\e[1;32m🎉 All $files files were checked successfully\e[0m"
     exit 0
 fi
