665258c87207593d41a8b96a7f1233a072d82c62
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2024-06-05 03:55:14 +0000

    Add ponyc to path

--- .ci-scripts/setup-ponyc.bash
+++ .ci-scripts/setup-ponyc.bash
@@ -6,5 +6,6 @@ set -u
 export SHELL=/bin/bash
 
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
-export PATH=/home/runner/.local/share/ponyup/bin:$PATH
+export PATH=$HOME/.local/share/ponyup/bin:$PATH
 ponyup update ponyc release
+export PATH=$HOME/.local/share/ponyup/bin/ponyc:$PATH
